33a99c392d83b6e20a2aaf5b13323ef1150f0b49
Andrew Glago <andrewglago1@gmail.com>
2021-12-02 18:15:19 +0000

    cf_02

--- control_flow_02/src/main.rs
+++ control_flow_02/src/main.rs
@@ -1,4 +1,26 @@
 fn main() {
+    labelled_loop();
+    for_loop();
+    range_loop()
+}
+
+fn range_loop() {
+    // This syntax is pretty cool considering py has the range function and js doesn't even have it builtin
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("Liftoff")
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("The value is: {}", element);
+    }
+}
+
+fn labelled_loop() {
     let mut count = 0;
 
     'counting_up: loop {
