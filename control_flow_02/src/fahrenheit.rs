518d3f3267ef6b6bb9ac3ea700beef0234e69dfa
Andrew Glago <andrewglago1@gmail.com>
2021-12-02 23:33:35 +0000

    fahrenheit -> celcius

---
+++ control_flow_02/src/fahrenheit.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn main() {
+  loop {
+    println!("Enter the Celcius temp you want to convert to Fahrenheit ");
+    let mut input = String::new();
+
+    // Collect input in terminal
+    io::stdin()
+      .read_line(&mut input)
+      .expect("Failed to read line");
+    let input: f32 = match input.trim().parse() {
+      Ok(val) => val,
+      Err(_) => {
+        println!("enter a number");
+        continue;
+      }
+    };
+
+    fn convert_to_fahrenheit(value: f32) -> f32 {
+      (value * 1.8) + 32 as f32
+    }
+
+    let fahrenheit_value = convert_to_fahrenheit(input);
+    println!(
+      "{} celcius converts to {} fahrenheit",
+      input, fahrenheit_value
+    );
+    break;
+  }
+}
