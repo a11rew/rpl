5cae395b42ed073edd2525385df2b05697fcdf96
Andrew Glago <andrewglago1@gmail.com>
2021-12-08 15:53:37 +0000

    multi param methods

--- ownership_03/methods.rs
+++ ownership_03/methods.rs
@@ -12,10 +12,29 @@ fn main() {
   println!("The area of the rectangle is {} square pixels",
     rect1.area()
   );
+
+  let rect2 = Rectangle {
+    width: 10,
+    height: 40,
+  };
+  let rect3 = Rectangle {
+    width: 60,
+    height: 45,
+  };
+
+  println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+  println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 impl Rectangle {    
   fn area(self: &Self) -> u32 {
     self.width * self.height
+  }
+
+  fn can_hold(&self, other_rect: &Rectangle) -> bool {
+    let me: u32 = self.area();
+    let other = other_rect.area();
+
+    me > other
   }
 }
