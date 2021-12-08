ec715fda7276de2bb485907c52b3fdf2c171d17d
Andrew Glago <andrewglago1@gmail.com>
2021-12-08 15:46:55 +0000

    methods

---
+++ ownership_03/methods.rs
@@ -0,0 +1,21 @@
+struct Rectangle {
+  width: u32,
+  height: u32
+}
+
+fn main() {
+  let rect1 = Rectangle {
+    width: 30,
+    height: 50
+  };
+
+  println!("The area of the rectangle is {} square pixels",
+    rect1.area()
+  );
+}
+
+impl Rectangle {    
+  fn area(self: &Self) -> u32 {
+    self.width * self.height
+  }
+}
